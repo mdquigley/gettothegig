312afbf46d0f48e0088f271a16b29af679a90ff4
mike <mdq204@gmail.com>
2021-07-27 19:34:40 -0400

    Add comments to TitleScene

--- src/TitleScene.js
+++ src/TitleScene.js
@@ -14,27 +14,46 @@ export default class TitleScene extends Phaser.Scene {
     }
 
     create() {
-        let windowWidth = this.cameras.main.width;
-        let windowHeight = this.cameras.main.height;
 
+        // Set variables for game window dimensions
+        let wWidth = this.cameras.main.width;
+        let wHeight = this.cameras.main.height;
+
+        // Set background for title scene
         this.cameras.main.setBackgroundColor('#c8f7f1');
 
-        this.title = this.add.sprite(windowWidth / 2, windowHeight * 0.3, 'titleImage');
+        // Add title graphic
+        this.title = this.add.sprite(wWidth / 2, wHeight * 0.3, 'titleImage');
         this.title.setScale(1);
 
+        // Add summary text
         let titleText = `Load-in was 30 minutes ago and your bandmates are nowhere to be found. Find all your gear and get to the gig!`
-        this.directions = this.add.text(windowWidth / 2, windowHeight * 0.55, titleText, { color: '#4aaf56', align: 'center', wordWrap: { width: windowWidth * 0.6, useAdvancedWrap: true } }).setOrigin(0.5);
+        this.summary = this.add.text(wWidth / 2, wHeight * 0.55, titleText, { color: '#4aaf56', align: 'center', wordWrap: { width: wWidth * 0.6, useAdvancedWrap: true } }).setOrigin(0.5);
 
+        // Add ENTER text to advance play
+        this.enter = this.add.text(wWidth / 2, wHeight * 0.7, "Press Enter to Play", { color: '#4aaf56', fontSize: '24px' }).setOrigin(0.5);
 
-        this.enter = this.add.text(windowWidth / 2, windowHeight * 0.7, "Press Enter to Play", { color: '#4aaf56' }).setOrigin(0.5);
+        /// Add navigation directions
+        this.directions = this.add.text(wWidth / 2, wHeight * 0.8, "Use W A S D keys to navigate", { color: '#4aaf56' }).setOrigin(0.5);
 
+        // Load sfx and set volume
         this.sfx = this.sound.add('blip');
         this.sfx.volume = 0.3;
 
+        // Event listener for Enter key
         this.input.keyboard.on('keyup', (e) => {
             if (e.key === 'Enter') {
+
+                // Start Tone (requires user action)
+                Tone.start();
+
+                // Play sfx
                 this.sfx.play();
+
+                // Advance to next scene
                 this.scene.start('Level01');
+
+                // Set tempo and start Tone.Transport
                 Tone.Transport.bpm.value = 180;
                 Tone.Transport.start();
             }
